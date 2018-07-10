3b0b992c68e6381303e90245a8103a8a83d21dc2
Tofabo <Tofabo@gmail.com>
2018-07-09 22:54:27 -0700

    chapter 3 excercises

--- chapter_3.js
+++ chapter_3.js
@@ -1,7 +1,58 @@
-console.log('hi');
-
-function hello () {
-    console.log('hi');
+// Using rest parameter to account for variable number of arguments
+// Rest parameter is an array! Array methods can be used.
+const amountAfterTax = (amount, ...taxes) => {
+    let percentage = taxes.reduce((x, y) => x + y, 0)
+    return (amount + (amount * percentage/100)).toFixed(2);
 }
 
-hello();
+const amount = 25.12;
+const fedTax = 10;
+const stateTax = 2;
+const localTax = 0.5;
+
+console.log(amountAfterTax(amount)); //25.12
+console.log(amountAfterTax(amount, fedTax)); //27.63
+console.log(amountAfterTax(amount, fedTax, stateTax)); //28.13
+console.log(amountAfterTax(amount, fedTax, stateTax, localTax)); //28.26
+
+// Using spread operator to concatonate two arrays
+const purchaseItems = function (essential1, essential2, ...optionals) {
+    console.log(essential1 + ', ' + essential2 + ', ' + optionals.join(', '));
+}
+
+purchaseItems('bread', 'milk');
+purchaseItems('bread', 'milk', 'jelly');
+
+const mustHaves = ['bread', 'milk'];
+const andAlso = ['eggs', 'donuts', 'tea'];
+
+purchaseItems(...mustHaves, ...andAlso);
+
+// Setting defaults in function declaration. 
+const purchaseMoreItems = function(essential1 = 'milk', essential2 = 'bread', ...optionals) {
+    console.log(essential1 + ', ' + essential2 + ', ' + optionals.join(', '));
+}
+const items = ['cheese', 'milk'];
+
+purchaseMoreItems('cheese');
+purchaseMoreItems(...items);
+purchaseMoreItems();
+
+// Setting default to value of a function! Oh snap.
+const placeOrder = function(id, amount, shipping = getShipping(amount), date = new Date()) {
+    console.log('shipping charge for id: ' + id + ' is $' + shipping + ' Date: ' + date.getDate());
+};
+
+const getShipping = function(amount) {
+    if (amount >= 20) {
+        return 10;
+    } else {
+        return 5;
+    }
+}
+
+placeOrder(1, 12.10, 3, new Date('05/15/2018'));
+placeOrder(1, 25.20, 10);
+placeOrder(1, 12.05);
+placeOrder(1, 25.30);
+placeOrder(1, 25.20);
