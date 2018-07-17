5a1b977b438ab7256e1f7419fbbda2ce113a1f46
Tofabo <Tofabo@gmail.com>
2018-07-16 22:38:26 -0700

    chapter 4, unfinished :(

---
+++ chapter_4.js
@@ -0,0 +1,83 @@
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+for (let i = 0; i < numbers.length; i = i +3) {
+    // console.log(numbers[i]);
+}
+
+for(const [i, number] of numbers.entries()) {
+    if( i%3 === 0 ) {
+        // console.log(number);
+    }
+} 
+
+console.log( 'The Symbol properties in arrays are: ');
+console.log(Object.getOwnPropertySymbols(Object.getPrototypeOf(numbers)));
+
+class Message {
+    constructor (text) {this.text = text;}
+
+    // My code here...
+    [Symbol.replace](og_string, value) {
+        console.log('this text', this.text);
+        console.log('og', og_string);
+        console.log('value', value);
+
+        return this.text.replace() // more code here)
+    }
+}
+
+const message = new Message('There are no stupid questions');
+
+console.log('stupid'.replace(message, 'S*****'));
+
+// console.log(''.replace(message, 'Yes, '));
+
+// My code here!
+
+const iterativeFib = function(index) {
+    let num_1 = 0;
+    let num_2 = 1;
+    let temp;
+    
+    if(index === 0) {
+        return num_1;
+    } else if (index === 1) {
+        return num_2;
+    }
+
+    for( let i = 1; i < index; i++) {
+        temp = num_2;
+        num_2 = num_2 + num_1;
+        num_1 = num_2;
+    }
+    return num_2;
+}
+
+const fibonocciSeries = function*(start = 0) {
+    let index = start;
+
+    while (true) {
+        if(iterativeFib(index)) yield iterativeFib(index);
+        index++;
+    }
+}
+
+for(const value of fibonocciSeries()){
+    if (value > 25 ) break;
+    // process.stdout.write(value + ", ");
+}
+
+const fibonocciSeries2 = function*(start = 0) {
+    let index = start;
+
+    while (true) {
+        if(iterativeFib(index)) yield iterativeFib(index);
+        index++;
+    }
+}
+
+for(const [index, value] of fibonocciSeries2()){
+    console.log(index);
+    if (index > 8 ) break;
+    process.stdout.write(value + ", ");
+}
